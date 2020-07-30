151a515102c1663de907ea932fb66413417c74bf
Aaron <aaron.birkett@yahoo.com>
2020-07-29 19:03:25 -0500

    final commit studio1

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
